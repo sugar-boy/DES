9db5bb4ed86de112c90504353ee6d7b6c7177121
Nikita Zhigulin <nikita1number@gmail.com>
2020-11-25 04:50:39 +0300

    update

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,7 @@ namespace DES
 
         private const int sizeOfBlock = 64; // размер принимаемого текста в битах
         private const int sizeOfChar = 8; // размер одного символа
+
         private readonly int[] firstArray = new int[64]
         {
             57, 49, 41, 33, 25, 17, 9 , 1, 59, 51, 43, 35, 27, 19, 11, 3,
@@ -66,6 +67,7 @@ namespace DES
             //24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1 ,
 
         };
+
         private readonly int[,] S_1 = new int[,]
         {
             {14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7 },
@@ -73,6 +75,55 @@ namespace DES
             { 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0 },
             {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13 }
         };
+        private readonly int[,] S_2 = new int[,]
+{
+            {15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10 },
+            { 3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5 },
+            { 0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15 },
+            {13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9 }
+};
+        private readonly int[,] S_3 = new int[,]
+{
+            {10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8 },
+            {13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1 },
+            {13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7 },
+            { 1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12 }
+};
+        private readonly int[,] S_4 = new int[,]
+{
+            { 7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15 },
+            {13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9 },
+            {10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4 },
+            { 3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14}
+};
+        private readonly int[,] S_5 = new int[,]
+{
+            { 2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9 },
+            {14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6 },
+            { 4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14 },
+            {11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3 }
+};
+        private readonly int[,] S_6 = new int[,]
+{
+            {12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11 },
+            {10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8 },
+            { 9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6 },
+            { 4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13 }
+};
+        private readonly int[,] S_7 = new int[,]
+{
+            { 4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1 },
+            {13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6 },
+            { 1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2 },
+            { 6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12 }
+};
+        private readonly int[,] S_8 = new int[,]
+{
+            {13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7 },
+            { 1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2 },
+            { 7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8 },
+            { 2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11 }
+};
 
         public Form1()
         {
@@ -84,7 +135,6 @@ namespace DES
             string text = Convert.ToString(textBox1.Text);
             string key = Convert.ToString(textBox2.Text);
             string result = "";
-            string cnt = "";
 
             CorrectStringLength(ref text);
             CorrectStringLength(ref key);
@@ -107,14 +157,18 @@ namespace DES
                 PermutationArray(divisionRightText, fourthArray, out string result_R);
 
                 text = XOR(result_K, result_R);
-
                 for (int j = 1; j <= 8; j++)
                 {
-                    cnt = text.Substring(n, 6);
-                    int number = S_1[cnt[0], cnt[5]];
-                    result += Convert.ToString(number, 2);
+                    string line = Convert.ToString(text[n] + "" + text[n + 5]);
+                    long l = Convert.ToInt64(line, 2);
+
+                    string row = text.Substring(n + 1, 4);
+                    long r = Convert.ToInt64(row, 2);
                     n += 6;
+
+                    result += Switch(j, l, r);
                 }
+                textBox4.Text = result;
 
             }
 
@@ -199,6 +253,41 @@ namespace DES
                     result += "1";
                 else
                     result += "0";
+            }
+            return result;
+        }
+
+        private string Switch(int number, long l, long r)
+        {
+            string result = "";
+            switch(number)
+            {
+                case 1:
+                    result = Convert.ToString(S_1[l, r] + " ");
+                    break;
+                case 2:
+                    result = Convert.ToString(S_2[l, r] + " ");
+                    break;
+                case 3:
+                    result = Convert.ToString(S_3[l, r] + " ");
+                    break;
+                case 4:
+                    result = Convert.ToString(S_4[l, r] + " ");
+                    break;
+                case 5:
+                    result = Convert.ToString(S_5[l, r] + " ");
+                    break;
+                case 6:
+                    result = Convert.ToString(S_6[l, r] + " ");
+                    break;
+                case 7:
+                    result = Convert.ToString(S_7[l, r] + " ");
+                    break;
+                case 8:
+                    result = Convert.ToString(S_8[l, r] + " ");
+                    break;
+                default:
+                    break;
             }
             return result;
         }
