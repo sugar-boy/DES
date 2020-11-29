80f184f9b03cf4f1e22c6ff2124510dac45f6536
Nikita Zhigulin <nikita1number@gmail.com>
2020-11-29 19:49:36 +0300

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,11 +36,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(426, 65);
+            this.textBox1.Location = new System.Drawing.Point(513, 57);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(254, 27);
             this.textBox1.TabIndex = 0;
@@ -48,7 +52,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(426, 129);
+            this.button1.Location = new System.Drawing.Point(513, 121);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 27);
             this.button1.TabIndex = 1;
@@ -58,7 +62,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(426, 96);
+            this.textBox2.Location = new System.Drawing.Point(513, 88);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(254, 27);
             this.textBox2.TabIndex = 0;
@@ -66,14 +70,14 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(426, 162);
+            this.textBox3.Location = new System.Drawing.Point(513, 154);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(254, 27);
             this.textBox3.TabIndex = 0;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(556, 129);
+            this.button2.Location = new System.Drawing.Point(643, 121);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(124, 27);
             this.button2.TabIndex = 2;
@@ -84,7 +88,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(295, 68);
+            this.label1.Location = new System.Drawing.Point(382, 60);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 20);
             this.label1.TabIndex = 3;
@@ -93,7 +97,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(367, 99);
+            this.label2.Location = new System.Drawing.Point(454, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 20);
             this.label2.TabIndex = 4;
@@ -102,17 +106,57 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(277, 165);
+            this.label3.Location = new System.Drawing.Point(364, 157);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(143, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Полученный текст: ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(74, 60);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(254, 27);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ключ: ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 27);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Зашифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(204, 93);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 27);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Расшифровать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -138,6 +182,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +27,7 @@ namespace DES
             //57, 49, 41, 33, 25, 17, 9 , 1, 59, 51, 43, 35, 27, 19, 11, 3,
             //61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7
 
-        }; // массив начальной перестановки битов сообщения
+        }; // массив начальной перестановки битов текста    
         private readonly int[] secondArray = new int[56]
         {
             56, 48, 40, 32, 24, 16, 8 , 0 , 57, 49, 41, 33, 25, 17,
@@ -39,7 +40,7 @@ namespace DES
             //63, 55, 47, 39, 31, 23, 15, 7 , 62, 54, 46, 38, 30, 22,
             //14, 6 , 61, 53, 45, 37, 29, 21, 13, 5 , 28, 20, 12, 4 ,
 
-        }; // массив начальной перестановки битов ключа
+        }; // массив для преобразования ключа шифрования
         private readonly int[] thirdArray = new int[48]
         {
             13, 16, 10, 23, 0 , 4 , 2 , 27, 14, 5 , 20, 9 ,
@@ -86,13 +87,14 @@ namespace DES
             //34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9 , 49, 17, 57, 25
         };
 
+        /// S-блоки
         private readonly int[,] S_1 = new int[,]
         {
             {14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7 },
             { 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8 },
             { 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0 },
             {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13 }
-        };
+        }; 
         private readonly int[,] S_2 = new int[,]
 {
             {15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10 },
@@ -153,8 +155,89 @@ namespace DES
             textBox3.Clear();
             string text = Convert.ToString(textBox1.Text);
             string key = Convert.ToString(textBox2.Text);
+            textBox3.Text = Encrypt(text, key);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            string text = Convert.ToString(textBox1.Text);
+            string key = Convert.ToString(textBox2.Text);
+            textBox3.Text = Decrypt(text, key);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            MessageBox.Show("Выберите файл с исходным текстом");
+            string text = "";
+            string key = textBox4.Text;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filenameRead = openFileDialog.FileName;
+                text = File.ReadAllText(filenameRead);
+            }
+            
+
+            text = Encrypt(text, key);
+            MessageBox.Show("Текст зашифрован, сохраняем в файл");
+
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filenameWrite = saveFileDialog.FileName;
+            System.IO.File.WriteAllText(filenameWrite, text);
+            MessageBox.Show("Зашифрованный текст сохранен в файл");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            MessageBox.Show("Выберите файл с зашифрованным текстом");
+            string text = "";
+            string key = textBox4.Text;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filenameRead = openFileDialog.FileName;
+                text = File.ReadAllText(filenameRead);
+            }
+
+            text = Decrypt(text, key);
+            MessageBox.Show("Текст расшифрован, сохраняем в файл");
+
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filenameWrite = saveFileDialog.FileName;
+            System.IO.File.WriteAllText(filenameWrite, text);
+            MessageBox.Show("Полученный текст сохранен в файле");
+
+
+        }
+
+
+        private string Encrypt(string text, string key) // метод шифровки текст
+        {
             string cnt;
             int[] result = new int[8];
+            string resultEnd = "";
 
             CorrectLength(ref text, ref key);
             textBox1.Text = text;
@@ -162,7 +245,7 @@ namespace DES
 
             string[] rounds = Rounds(text);
 
-            for(int k = 0; k < rounds.Length; k++ )
+            for (int k = 0; k < rounds.Length; k++)
             {
                 text = rounds[k];
 
@@ -211,18 +294,15 @@ namespace DES
                 PermutationArray(cnt, sixArray, out string resultLR);
 
                 long[] dnt = ConvertDecimical(resultLR, 8);
-                textBox3.Text += ConvertASCII(dnt);
+                resultEnd += ConvertASCII(dnt);
             }
-            
-        }
-
-        private void button2_Click(object sender, EventArgs e)
+            return resultEnd;
+        } 
+        private string Decrypt(string text, string key) // метод расшифровки текст
         {
-            textBox3.Clear();
-            string text = Convert.ToString(textBox1.Text);
-            string key = Convert.ToString(textBox2.Text);
             string cnt;
             int[] result = new int[8];
+            string resultEnd = "";
 
             string[] rounds = Rounds(text);
 
@@ -275,12 +355,11 @@ namespace DES
                 PermutationArray(cnt, sixArray, out string resultLR);
 
                 long[] dnt = ConvertDecimical(resultLR, 8);
-                textBox3.Text += ConvertASCII(dnt);
+                resultEnd += ConvertASCII(dnt);
             }
-
+            return resultEnd;
         }
-
-        private void CorrectLength(ref string text, ref string key) // доводим текст до требуемой длиныkey
+        private void CorrectLength(ref string text, ref string key) // доводим текст до требуемой длины
         {
             while ((text.Length * 8) % sizeOfBlock != 0)
                 text += "*";
@@ -295,7 +374,6 @@ namespace DES
                     key += "*";
             }
         }
-
         private int[] ConvertASCII(string text) // переводим символ в ASCII
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
@@ -310,7 +388,6 @@ namespace DES
             }
             return array;
         }
-
         private string ConvertASCII(long[] array) // переводим символ в ASCII
         {
             byte[] bytes = new byte[array.Length];
@@ -321,7 +398,7 @@ namespace DES
 
             return Encoding.GetEncoding(1251).GetString(bytes);
         }
-        private string ConvertBinary(int[] arrayASCII, int sizeOfChar) // конвертируем строку в 2-ичный код
+        private string ConvertBinary(int[] arrayASCII, int sizeOfChar) // конвертируем строку в 2-ую систему счисления
         {
             string result = "";
 
@@ -336,8 +413,7 @@ namespace DES
             }
             return result;
         }
-
-        private long[] ConvertDecimical(string text, int sizeOfChar) // конвертируем строку в 2-ичный код
+        private long[] ConvertDecimical(string text, int sizeOfChar) // конвертируем строку в 10-ую систему счисления
         {
             long[] result = new long[8];
             int cnt = 0;
@@ -348,8 +424,7 @@ namespace DES
             }
             return result;
         }
-
-        private void PermutationArray(string text, int[] array, out string result) // делаем 1-ую перестановку ключа по второму массиву
+        private void PermutationArray(string text, int[] array, out string result) // метод, реализующий перестановку
         {
             result = "";
             for (int i = 0; i < array.Length; i++)
@@ -357,13 +432,11 @@ namespace DES
                 result += text[array[i]];
             }
         }
-
-        private void Division(string text, out string divisionLeft, out string divisionRight) // делим последовательность на 2 равные части
+        private void Division(string text, out string divisionLeft, out string divisionRight) // метод, разделяющий входной текст на 2 последовательности
         {
             divisionLeft = text.Substring(0, text.Length / 2);
             divisionRight = text.Substring(text.Length / 2, text.Length / 2);
         }
-
         private string KeyBitShift(string text, int number)
         {
             string result = "";
@@ -380,8 +453,7 @@ namespace DES
             }
             return result;
         } // сдвиг битов
-
-        private string XOR(string one, string two)
+        private string XOR(string one, string two) // операция XOR
         {
             string result = "";
 
@@ -397,8 +469,7 @@ namespace DES
             }
             return result;
         }
-
-        private string Switch(int number, long l, long r)
+        private string Switch(int number, long l, long r) // метод, осуществляющий перестановку битов по S-блокам
         {
             string result = "";
             switch(number)
@@ -432,8 +503,7 @@ namespace DES
             }
             return result;
         }
-
-        private string[] Rounds(string text)
+        private string[] Rounds(string text) // метод, вычисляющий количество раундов шифрования
         {
             string[] blocks = new string[text.Length / 8];
             int cnt = 0;
@@ -445,7 +515,5 @@ namespace DES
             }
             return blocks;
         }
-
-        
     }
 }
